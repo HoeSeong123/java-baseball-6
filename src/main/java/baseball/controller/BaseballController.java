afd40244abaaec9ce82b9db66fcfbbd33c4962db
HoeSeong123 <ghltjd3@gmail.com>
2023-12-12 00:11:32 +0900

    feat : 게임 시작 문구를 출력한다

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,9 @@
+package baseball.controller;
+
+import baseball.view.OutputView;
+
+public class BaseballController {
+    public void run() {
+        OutputView.printGameStartMessage();
+    }
+}
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,7 @@
+package baseball.view;
+
+public class OutputView {
+    public static void printGameStartMessage() {
+        System.out.println("숫자 야구 겡미을 시작합니다.");
+    }
+}
